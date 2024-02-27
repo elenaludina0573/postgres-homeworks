7324859d1961ae5d1e544a7c0c55431e3af2cede
elenaludina0573 <elenaludina0@gmail.com>
2024-02-27 20:07:02 +0300

    Update filter_sorting.sql_2

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -12,4 +12,4 @@ SELECT AVG(shipped_date - order_date) FROM orders WHERE ship_country = 'Germany'
 SELECT MIN(unit_price), MAX(unit_price) FROM products WHERE discontinued <> 1
 
 -- 5. минимальную и максимальную цену среди продуктов, не снятых с продажи и которых имеется не меньше 20 (таблица products, колонки unit_price, units_in_stock, discontinued не равно 1)
-SELECT MIN(unit_price), MAX(unit_price) FROM products WHERE units_in_stock > 20, discontinued <> 1
+SELECT MIN(unit_price), MAX(unit_price) FROM products WHERE units_in_stock > 20 AND discontinued <> 1
